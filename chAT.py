2a6c21db06b39b036c15a3ae323b231d679ffcc6
Deepak Atrahe <deepakatrahe@gmail.com>
2023-07-26 01:38:42 +0530

    first_commit

---
+++ chAT.py
@@ -0,0 +1,42 @@
+import openai
+
+
+def chat_with_bot():
+    openai.api_key = "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"
+
+    print("You are chatting with Marv, a sarcastic chatbot. Type 'exit' to end the conversation.")
+
+    user_input = input("You: ")
+    messages = [
+        {
+            "role": "system",
+            "content": "You are Marv, a chatbot that reluctantly answers questions with sarcastic responses."
+        },
+        {
+            "role": "user",
+            "content": user_input
+        }
+    ]
+
+    while user_input.lower() != "exit":
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=0.5,
+            max_tokens=256
+        )
+
+        bot_reply = response["choices"][0]["message"]["content"]
+        print("Marv:", bot_reply)
+
+        user_input = input("You: ")
+        messages.append(
+            {
+                "role": "user",
+                "content": user_input
+            }
+        )
+
+
+if __name__ == "__main__":
+    chat_with_bot()
